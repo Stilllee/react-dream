42141a7b5a2e17591e5207848aa5eb39bfb6ad40
Stilllee <orodae@gmail.com>
2024-02-07 00:05:05 +0900

    Profile 컴포넌트 생성

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <img
+        className="photo"
+        src="https://images.unsplash.com/photo-1549825180-42d5efa257a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fCVFQyVCOSU5OCVFQyU5OSU4MCVFQyU5OSU4MHxlbnwwfHwwfHx8MA%3D%3D"
+        alt="Chihuahua"
+      />
+      <h1>치와와</h1>
+      <p>귀여운 소형견</p>
+    </div>
+  );
+}
