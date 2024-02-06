ad1cb334e63325bdac64ad72fd89684126f166ea
Stilllee <orodae@gmail.com>
2024-02-07 00:14:33 +0900

    Props 사용

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,7 +4,21 @@ import Profile from "./components/Profile";
 export default function AppProfile() {
   return (
     <>
-      <Profile />
+      <Profile
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnva795U6Ke5YxLOwi3LE3o7gGynyfXmf6EXuoG9ay7l9MqOa-UA79-gqXu3rw12jWyzA&usqp=CAU"
+        name="푸바오"
+        title="푸린세스"
+      />
+      <Profile
+        image="https://i.namu.wiki/i/AglvF-QeqoTIBd8QBzVeqrTV6Ym2R9bfsxOE-FfWgQlQE4DpW-Xhm56deIxZfpED6tcEmerUHBhLGScgQUfSmw.webp"
+        name="아이바오"
+        title="아여사"
+      />
+      <Profile
+        image="https://i.namu.wiki/i/Urvpd8COTqmf5PORAJ7rLEjSgLXvcFjJlAvfvkm8bnbUXr-c3eLDO1OsnOPyftZ6JdtrdpHRa_ImcoW8eWEqvfBnRjUaofr2AZ6HSRjny-mhRLN1n5EWYPPcDuI3Vaur56vrYGo5131NQu9F81xUrg.webp"
+        name="러바오"
+        title="낭만판다"
+      />
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-export default function Profile() {
+export default function Profile({ image, name, title }) {
   return (
     <div className="profile">
-      <img
-        className="photo"
-        src="https://images.unsplash.com/photo-1549825180-42d5efa257a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fCVFQyVCOSU5OCVFQyU5OSU4MCVFQyU5OSU4MHxlbnwwfHwwfHx8MA%3D%3D"
-        alt="Chihuahua"
-      />
-      <h1>치와와</h1>
-      <p>귀여운 소형견</p>
+      <img className="photo" src={image} alt="avatar" />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
