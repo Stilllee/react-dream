5bde79ed114c4b6569bedf04a1af76cbdbfb5d02
Stilllee <orodae@gmail.com>
2024-02-07 22:44:42 +0900

    [마우스 따라가기] 개선하기
    - 이전 상태의 값을 기억하면서 특정 값만 변경하기

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
   return (
     <div
       className="container"
       onPointerMove={(e) => {
         console.log(e.clientX, e.clientY);
-        setPosition({ x: e.clientX, y: e.clientY });
+        // setPosition({ x: e.clientX, y: e.clientY });
+        // 수평으로만 이동이 가능하게 하려면?
+        setPosition((prev) => ({ ...prev, x: e.clientX }));
       }}
     >
       <div
