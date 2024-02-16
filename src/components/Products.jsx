edcb82f57a1f0944b26628411be129db01115c19
Stilllee <orodae@gmail.com>
2024-02-16 17:32:22 +0900

    업데이트 하기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,12 +10,12 @@ export default function Products() {
   } = useQuery({
     queryKey: ["products", checked],
     queryFn: async () => {
-      console.log("fetching...");
+      console.log("fetching...", checked);
       return fetch(`/data/${checked ? "sale_" : ""}products.json`).then((res) =>
         res.json()
       );
     },
-    staleTime: 5000,
+    staleTime: 1000 * 60 * 5,
   });
   const handleChange = () => setChecked((prev) => !prev);
 
