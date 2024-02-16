990476fe4c2214a8d7c29cb84dc57dbbab1ddc4e
Stilllee <orodae@gmail.com>
2024-02-16 17:07:07 +0900

    react query devtools

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import MainProducts from "./components/MainProducts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
